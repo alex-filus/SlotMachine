26ad75d6d099fc2b920061f2d078c5df66d85234
Maluska10 <salamona@csp.edu>
2025-05-05 21:51:34 -0500

    Started a code for win options.

--- Program.cs
+++ Program.cs
@@ -6,14 +6,14 @@ namespace SlotMachine
     {
         static void Main(string[] args)
         {
-
+            int wagerInt = 0;
 
 
             Console.WriteLine("Welcome to the Slot Machine!");
             Console.WriteLine();
 
             Console.WriteLine("Enter your wager.");
-            
+
             //if wager is not an integer, ask again
 
             while (true)
@@ -21,11 +21,11 @@ namespace SlotMachine
             {
                 string wager = Console.ReadLine();
 
-                
+
 
                 if (wager.All(char.IsDigit))
                 {
-                    int wagerInt = Convert.ToInt32(wager);
+                    wagerInt = Convert.ToInt32(wager);
                     break;
                 }
 
@@ -33,52 +33,73 @@ namespace SlotMachine
                 {
                     Console.WriteLine("Wrong input. Try again.");
                 }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Press SPACE to start");
+            Console.WriteLine();
+
+
+            Random random = new Random();
+            int[,] slotGrid = new int[3, 3];
+
+            while (true)
+            {
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+
+                //if user enters SPACE, proceed with generating the grid of random numbers
+                if (keyInfo.Key == ConsoleKey.Spacebar)
+                {
+                    for (int i = 0; i < 3; i++)
+
+                    {
+
+                        for (int j = 0; j < 3; j++)
+                        {
+                            slotGrid[i,j] = random.Next(1, 9);
+                            Console.Write(slotGrid[i,j] + " ");
+                        }
+                        Console.WriteLine();
+
+                    }
                 }
 
-                Console.WriteLine();
-                Console.WriteLine("Press SPACE to start");
-                Console.WriteLine();
 
-
-                Random random = new Random();
-
-
-                while (true)
+                else
                 {
-                    ConsoleKeyInfo keyInfo = Console.ReadKey(true);
-
-                    //if user enters SPACE, proceed with generating the grid of random numbers
-                    if (keyInfo.Key == ConsoleKey.Spacebar)
-                    {
-                        for (int i = 0; i < 3; i++)
-
-                        {
-
-                            for (int j = 0; j < 3; j++)
-                            {
-                                int randomNumber = random.Next(1, 9);
-                                Console.Write(randomNumber + " ");
-                            }
-                            Console.WriteLine();
-
-                        }
-                    }
-
-
-                    else
-                    {
-                        Console.WriteLine("Please press SPACE instead.");
-                    }
+                    Console.WriteLine("Please press SPACE instead.");
                 }
             }
 
+            //Win options
+            int win1 = 0;
+            int win2 = 0;
+            int win3 = 0;
+            int win4 = 0;
 
 
+            if (slotGrid[0,0] == slotGrid[1,1] && slotGrid[1,1] == slotGrid[2,2])
+            {
+                win1 = 1;
+            }
 
+            if (slotGrid[0, 2] == slotGrid[1, 1] && slotGrid[1, 1] == slotGrid[2, 0])
+            {
+                win2 = 1;
 
+            }
 
+           
 
         }
+
+
+
+
+
+
+
     }
+}
 
 
