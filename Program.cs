4e4a59c9d8cc908ee773e6a4ae9236501a3bc150
Maluska10 <salamona@csp.edu>
2025-05-18 20:53:06 -0500

    Added logic to all cases, added balance calculation.

--- Program.cs
+++ Program.cs
@@ -6,14 +6,26 @@ namespace SlotMachine
     {
         static void Main(string[] args)
         {
-            int wagerInt = 0;
+            //testing grid
+            int[,] slotGrid =
+        {
+    { 1, 5, 1 },
+    { 0, 1, 2 },
+    { 1, 0, 2 }
+        };
+
+
             const int ROWS = 3;
             const int COLUMNS = 3;
+            const int CENTRAL_LINE = 1;
+            const int HORIZONTAL_LINES = 2;
+            const int VERTICAL_LINES = 3;
+            const int DIAGONAL_LINES = 4;
+            int balance = 0;
+            int wagerInt = 0;
 
             Console.WriteLine("Welcome to the Slot Machine!");
             Console.WriteLine();
-
-           
 
             int gameChoice = 0;
             while (true)
@@ -29,7 +41,7 @@ namespace SlotMachine
 
                 string userInput = Console.ReadLine();
 
-                if (int.TryParse(userInput, out gameChoice) && gameChoice >= 1 && gameChoice <= 4)
+                if (int.TryParse(userInput, out gameChoice) && gameChoice >= CENTRAL_LINE && gameChoice <= DIAGONAL_LINES)
                 {
                     break;
                 }
@@ -50,13 +62,12 @@ namespace SlotMachine
             {
                 string wager = Console.ReadLine();
 
-
-
+                wagerInt = 0;
                 if (wager.All(char.IsDigit))
                 {
                     wagerInt = Convert.ToInt32(wager);
                     Console.WriteLine($"Betting ${wagerInt}?");
-                   
+
                     break;
                 }
 
@@ -65,16 +76,15 @@ namespace SlotMachine
                     Console.WriteLine("Wrong input. Try again.");
                 }
             }
-            
-           
+
             Console.WriteLine("Press SPACE to start the game.");
 
-            Random random = new Random();
-            int[,] slotGrid = new int[3, 3];
+            //Random random = new Random();
+            //int[,] slotGrid = new int[ROWS, COLUMNS];
 
             while (true)
             {
-                
+
                 ConsoleKeyInfo keyInfo = Console.ReadKey(true);
 
                 //if user enters SPACE, proceed with generating the grid of random numbers
@@ -83,92 +93,159 @@ namespace SlotMachine
                     for (int i = 0; i < ROWS; i++)
 
                     {
-
                         for (int j = 0; j < COLUMNS; j++)
                         {
-                            slotGrid[i,j] = random.Next(1, 9);
-                            Console.Write(slotGrid[i,j] + " ");
+                            Console.Write(slotGrid[i, j] + " ");
+                            //slotGrid[i,j] = random.Next(1, 9);
+                            //Console.Write(slotGrid[i,j] + " ");
                         }
                         Console.WriteLine();
-
                     }
                     break;
                 }
-                
 
                 else
                 {
                     Console.WriteLine("Please press SPACE instead.");
                 }
-                
-            }
 
-            
+            }
 
             switch (gameChoice)
             {
-               case 1: //center line option
-                    bool centerLineWin = true;
-                    for (int i = 0; i < ROWS - 1; i++)
+                case 1: //center line option
+                    int middleLine = ROWS / 2;
+                    bool centerWin = true;
+
+                    for (int i = middleLine; i < ROWS; i++)
                     {
-                        if (slotGrid[i,1] != slotGrid[i + 1, 1])
+                        if (slotGrid[middleLine, 0] != slotGrid[middleLine, i])
                         {
+                            centerWin = false;
                             Console.WriteLine("No match.");
+                            balance -= wagerInt;
                             break;
                         }
                         else
                         {
                             Console.WriteLine("You win!");
+                            balance += wagerInt * 2;
                         }
                     }
                     break;
-                case 2:
-                    if (slotGrid[0, 0] == slotGrid[1, 0] && slotGrid[1, 0] == slotGrid[2, 0]
-                        || slotGrid[0, 1] == slotGrid[1, 1] && slotGrid[1,1] == slotGrid[2, 1]
-                        || slotGrid[0,2] == slotGrid[1,2] && slotGrid[1,2] == slotGrid[2,2])
+
+                case 2: //all horizontal lines option
+
+                    bool horizontalWin = true;
+                    for (int i = 0; i < ROWS; i++)
                     {
-                        Console.WriteLine("All horizontal lines match!");
+                        for (int j = 1; j < COLUMNS; j++)
+                        {
+                            if (slotGrid[i, 0] != slotGrid[i, j])
+                            {
+                                horizontalWin = false;
+                                break;
+                            }
+                        }
+                        if (horizontalWin)
+                        {
+                            Console.WriteLine("You win!");
+                            balance += wagerInt * 2;
+                            break;
+                        }
                     }
-                    else
+                    if (!horizontalWin)
                     {
-                        Console.WriteLine("Sorry, you lose.");
+                        Console.WriteLine($"No match.");
+                        balance -= wagerInt;
                     }
                     break;
-                case 3:
-                    if (slotGrid[0,0] == slotGrid[0,1] && slotGrid[0,1] == slotGrid[0,2]
-                        || slotGrid[1,0] == slotGrid[1,1] && slotGrid[1,1] == slotGrid[1,2]
-                        || slotGrid[2,0] == slotGrid[2,1] && slotGrid[2,1] == slotGrid[2,2])
+
+                case 3: //all vertical lines option
+
+                    bool verticalWin = true;
+                    for (int i = 0; i < ROWS; i++)
                     {
-                        Console.WriteLine("All vertical lines match!");
+                        for (int j = 0; j < COLUMNS; j++)
+                        {
+                            if (slotGrid[0, j] != slotGrid[i, j])
+                            {
+                                verticalWin = false;
+                                break;
+                            }
+                        }
+                        if (verticalWin)
+                        {
+                            Console.WriteLine("You win!");
+                            balance += wagerInt * 2;
+                            break;
+                        }
                     }
-                    else
+                    if (!verticalWin)
                     {
-                        Console.WriteLine("Sorry, you lose.");
+                        Console.WriteLine($"No match.");
+                        balance -= wagerInt;
                     }
                     break;
-                case 4:
-                    if (slotGrid[0,0] == slotGrid[1,1] && slotGrid[1,1] == slotGrid[2,2]
-                        || slotGrid[0,2] == slotGrid[1,1] && slotGrid[1,1] == slotGrid[2,0])
+
+                case 4: //Diagonal lines
+
+                    bool diagonalWin1 = true;
+                    for (int i = 1; i < ROWS; i++)
+                    {
+
+                        if (slotGrid[0, 0] != slotGrid[i, i])
+                        {
+                            diagonalWin1 = false;
+                            break;
+                        }
+                    }
+
+                    if (diagonalWin1)
                     {
                         Console.WriteLine("You win!");
+                        balance += wagerInt * 2;
+                        break;
                     }
-                    else
+
+                    bool diagonalWin2 = true;
+                    for (int j = 1; j < COLUMNS; j++)
                     {
-                        Console.WriteLine("Sorry, you lose.");
+                        
+                        if (slotGrid[ROWS - 1, 0] != slotGrid[COLUMNS - 1 - j, j])
+                        {
+                            diagonalWin2 = false;
+                            break;
+                        }
                     }
+
+                    if (diagonalWin2)
+                    {
+                        Console.WriteLine("You win!");
+                        balance += wagerInt * 2;
+                        break;
+                    }
+
+                    if (!diagonalWin1 && !diagonalWin2)
+                    {
+                        Console.WriteLine($"No match.");
+                        balance -= wagerInt;
+                    }
+
                     break;
+
             }
+
+            Console.WriteLine($"Your current balance is ${balance}");
+
+
+
+
+
 
 
 
         }
-
-
-
-
-
-
-
     }
 }
 
